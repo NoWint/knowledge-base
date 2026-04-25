9ba8122ccf035cd0b2f6d47f5a2c8a309d4807e8
Xiatian <x674000249@gmail.com>
2026-04-25 15:30:56 +0800

    fix: 完善数据文件复制，修复basePath配置

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
   reactStrictMode: true,
   compress: true,
   trailingSlash: true,
   output: 'export',
-  basePath: '/knowledge-base',
+  basePath: isProd ? '/knowledge-base' : '',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   experimental: {
